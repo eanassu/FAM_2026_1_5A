cb203022afc830411042a1dc441342d7cadfa38f
052787 <052787@DKACAD310F-01.famacad.com.br>
2026-03-26 20:40:40 -0300

    início do controller de Funcionarios

---
+++ src/main/java/br/com/vemprafam/controller/FuncionariosController.java
@@ -0,0 +1,37 @@
+package br.com.vemprafam.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.vemprafam.dao.DaoFuncionario;
+import br.com.vemprafam.pojo.Funcionario;
+
+@Controller
+@RequestMapping("/funcionarios")
+public class FuncionariosController {
+	@GetMapping
+	public String showFuncHomeVazio(Model model) {
+		return "funcionarios";
+	}
+	@GetMapping("/")
+	public String showFuncHome(Model model) {
+		return "funcionarios";
+	}
+	@GetMapping("/new")
+	public String showFuncForm(Model model) {
+		Funcionario f = new Funcionario();
+		model.addAttribute("funcionario", f);
+		return "create-func";
+	}
+	DaoFuncionario dao = new DaoFuncionario();
+	@PostMapping
+	public String insert(@ModelAttribute Funcionario f) {
+		dao.insert(f);
+		return "redirect:funcionarios";
+	}
+
+}
